bc20a8d758cbd8e7db8cb89cb98c24677c1350a7
eziz-tajiev <eziztajiev@gmail.com>
2024-12-20 20:40:54 +0500

    add props to SearchBtn component

--- src/components/SearchBtn.tsx
+++ src/components/SearchBtn.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { IoSearch } from "react-icons/io5";
 
+type Props = {
+    value: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    onSubmit: React.FormEventHandler<HTMLFormElement>;
+    
+}
 
-export default function SearchBtn() {
+
+export default function SearchBtn(props: Props) {
     return (
-        <form className="flex items-center gap-2 shadow-md focus-within:ring-2 dark:focus-within:ring-gray-200
+        <form
+         onSubmit={props.onSubmit}
+         className="flex items-center gap-2 shadow-md focus-within:ring-2 dark:focus-within:ring-gray-200
          focus-within:ring-slate-800 p-2 rounded-lg dark:bg-slate-800 bg-white  mx-2">
             <section className="flex items-center h-full gap-2 w-full"> 
                 {/* search icon */}
                 <IoSearch className="text-2xl text-blue-500" />
             <input
+            value={props.value}
+            onChange={props.onChange}
             placeholder="Search Github username..." 
             className="w-full h-[40px] rounded bg-inherit outline-none px-1 text-sm"
             type="text" />
